917a07e556cc2081d9285679b6f01391fe5f9bf2
Enzo Hung <enzo.brightsoft@gmail.com>
2026-04-13 19:52:31 +0700

    update code 2

--- ExamOne/wwwroot/js/site.js
+++ ExamOne/wwwroot/js/site.js
@@ -1,16 +1,81 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    const defaultImg = "/img/user.jpg";
+﻿let isDev = false;
 
-    document.querySelectorAll("img").forEach(img => {
+if (!isDev) {
+    document.addEventListener("DOMContentLoaded", () => {
+        const defaultImg = "/img/user.jpg";
 
-        img.onerror = function () {
-            if (this.src.endsWith(defaultImg)) return;
+        document.querySelectorAll("img").forEach(img => {
 
-            this.src = defaultImg;
-        };
+            img.onerror = function () {
+                if (this.src.endsWith(defaultImg)) return;
 
-        if (img.complete && img.naturalWidth === 0) {
-            img.src = defaultImg;
+                this.src = defaultImg;
+            };
+
+            if (img.complete && img.naturalWidth === 0) {
+                img.src = defaultImg;
+            }
+        });
+    });
+
+    document.addEventListener('keydown', function (e) {
+        if (
+            e.key === "F12" ||
+            (e.ctrlKey && e.shiftKey && e.key === "I") ||
+            (e.ctrlKey && e.shiftKey && e.key === "J") ||
+            (e.ctrlKey && e.key === "U")
+        ) {
+            e.preventDefault();
+            return false;
         }
     });
-});
+
+    const channel = new BroadcastChannel("exam_channel");
+
+    let isDuplicate = false;
+
+    channel.postMessage("CHECK");
+
+    channel.onmessage = (e) => {
+        if (e.data === "CHECK") {
+            channel.postMessage("EXISTS");
+        }
+
+        if (e.data === "EXISTS") {
+            isDuplicate = true;
+            alert("Bạn đang mở nhiều tab!");
+            window.close();
+        }
+    };
+
+    //console.log('123');
+
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+
+    //let devtoolsOpen = false;
+
+    //setInterval(function () {
+    //    const threshold = 160;
+    //    if (
+    //        window.outerWidth - window.innerWidth > threshold ||
+    //        window.outerHeight - window.innerHeight > threshold
+    //    ) {
+    //        if (!devtoolsOpen) {
+    //            devtoolsOpen = true;
+
+    //            // 👉 xử lý khi phát hiện mở DevTools
+    //            alert("DevTools detected!");
+
+    //            // redirect
+    //            window.location.href = "/";
+
+    //            // hoặc thử đóng tab (không phải lúc nào cũng work)
+    //            window.close();
+    //        }
+    //    } else {
+    //        devtoolsOpen = false;
+    //    }
+    //}, 1000);
+}
